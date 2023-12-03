f28886d439d0fe475e415185f315621eccfff08c
salahu01 <cvsalahu12@gmail.com>
2023-12-03 14:24:38 +0530

    added preview_x

--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,3 +1,4 @@
 export './dev_tools.dart';
 export './individual_network.dart';
 export './network.dart';
+export './preview_x.dart';
---
+++ lib/presentation/preview_x.dart
@@ -0,0 +1,42 @@
+import 'dart:core';
+import 'package:dev_tools/dev_tools.dart';
+import 'package:flutter/material.dart';
+
+class PreviewX extends StatelessWidget {
+  const PreviewX({
+    super.key,
+    required this.child,
+    this.devices,
+    this.data,
+    this.availableLocales,
+    this.defaultDevice,
+    required this.tools,
+    this.storage,
+    this.backgroundColor,
+  });
+  final Widget child;
+  final List<DeviceInfo>? devices;
+  final DevicePreviewData? data;
+  final bool isToolbarVisible = true;
+  final List<Locale>? availableLocales;
+  final DeviceInfo? defaultDevice;
+  final List<Widget> tools;
+  final DevicePreviewStorage? storage;
+  final bool enabled = true;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return DevicePreview(
+      enabled: DevToolsData.isEnabled,
+      tools: const [
+        DevTools(),
+        DeviceSection(),
+        SystemSection(),
+        AccessibilitySection(),
+        SettingsSection(),
+      ],
+      builder: (context) => child,
+    );
+  }
+}
